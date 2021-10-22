7e030c2b234e52507e72768f38bbe15752ef4042
Sergei Zaiaev <serjzv@gmail.com>
2021-10-22 17:24:42 +0300

    Logging

--- src/app.py
+++ src/app.py
@@ -1,8 +1,15 @@
+import logging
 import os
 from time import sleep
 
 from prometheus_client import Gauge, start_http_server
-from waldur_client import WaldurClient
+from waldur_client import WaldurClient, WaldurClientException
+
+handler = logging.StreamHandler()
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter("[%(levelname)s] [%(asctime)s] %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 WALDUR_API_URL = os.environ["WALDUR_API_URL"]
 WALDUR_API_TOKEN = os.environ["WALDUR_API_TOKEN"]
@@ -25,9 +32,26 @@ if __name__ == "__main__":
     )
 
     while True:
-        users_total.set(client.count_users())
-        customers_total.set(client.count_customers())
-        resources_total.set(client.count_marketplace_resources())
-        projects_total.set(client.count_projects())
+        try:
+            users_count = client.count_users()
+            customers_count = client.count_customers()
+            marketplace_resources_count = client.count_marketplace_resources()
+            projects_count = client.count_projects()
+
+            users_total.set(users_count)
+            customers_total.set(customers_count)
+            resources_total.set(marketplace_resources_count)
+            projects_total.set(projects_count)
+            logger.info(f"Total count of users: {users_count}")
+            logger.info(f"Total count of customers: {customers_count}")
+            logger.info(
+                f"Total count of marketplace_resources: {marketplace_resources_count}"
+            )
+            logger.info(f"Total count of projects: {projects_count}")
+            print()
+        except WaldurClientException as e:
+            logger.error(f"Unable to collect metrics. Message: {e}")
+        except Exception as e:
+            logger.error(f"Unable to collect metrics. Exception: {e}")
 
         sleep(10.0)
